463c2b4326ad2b371b42c839d2472cae4aca3293
Roger Wells <rsw359@gmail.com>
2022-07-27 17:24:15 +0900

    login test failed

--- ecommerce/dashboard/tests/test_selenium_dashboard.py
+++ ecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -1,7 +1,20 @@
 import pytest
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 @pytest.mark.selenium
-def test_dashboard_admin_login(chrome_browser_instance):
-  pass
+def test_dashboard_admin_login(live_server, chrome_browser_instance):
 
+  browser = chrome_browser_instance
+
+  browser.get(("%s%s" % (live_server.url, "/admin/login/")))
+
+  user_name = browser.find_element(By.NAME, "username")#finds input element on the page, find by inspecting
+  user_password = browser.find_element(By.NAME, "password")#finds input element on the page, find by inspecting
+  submit = browser.find_element(By.XPATH, '//input[@value="Log in"]')  #this element is a button
+
+  user_name.send_keys("admin")
+  user_password.send_keys("admin")
+  submit.send_keys(Keys.RETURN)
+
+  assert "Site administration" in browser.page_source
--- ecommerce/tests/selenium.py
+++ ecommerce/tests/selenium.py
@@ -6,12 +6,13 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope="module")
 def chrome_browser_instance(request):
   """
-  provide a selenium webdriver instance
+  provides a selenium webdriver instance
   """
+  driver = webdriver.Chrome(executable_path='/Users/roger/projects/e-commerce/django_tut_2/ecommerce/dashboard/tests/chromedriver')
 
   options = Options()
   options.headless = False 
-  """headless = True allows test to be viewed"""
+  """headless allows test to be viewed"""
   browser = webdriver.Chrome(chrome_options=options)
   yield browser
   browser.close()
