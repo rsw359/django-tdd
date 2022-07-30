f231d785c5bdadb1997e970a489e5df4e7ec7e39
Roger Wells <rsw359@gmail.com>
2022-07-30 10:43:29 +0900

    added black vscode settings json

--- ecommerce/tests/selenium.py
+++ ecommerce/tests/selenium.py
@@ -1,18 +1,22 @@
 import pytest
 from requests import options
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 @pytest.fixture(scope="module")
 def chrome_browser_instance(request):
-  """
-  provides a selenium webdriver instance
-  """
-  driver = webdriver.Chrome(executable_path='/Users/roger/projects/e-commerce/django_tut_2/ecommerce/dashboard/tests/chromedriver')
+    """
+    provides a selenium webdriver instance
+    """
+    driver = webdriver.Chrome(
+        executable_path="/Users/roger/projects/e-commerce/django_tut_2/ecommerce/dashboard/tests/chromedriver"
+    )
 
-  options = Options()
-  options.headless = False 
-  """headless allows test to be viewed"""
-  browser = webdriver.Chrome(chrome_options=options)
-  yield browser
-  browser.close()
+    options = Options()
+    options.headless = False
+    """headless allows test to be viewed"""
+    browser = webdriver.Chrome(chrome_options=options)
+    yield browser
+    browser.close()
