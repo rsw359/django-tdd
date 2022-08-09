3a1fedb4f6774524e4e91721c270c7162c236f32
Roger Wells <rsw359@gmail.com>
2022-08-09 15:53:07 +0900

    product table test added

--- ecommerce/inventory/tests/test_db_fixtures.py
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -2,6 +2,7 @@ from unittest import result
 
 import pytest
 from ecommerce.inventory import models
+from typing_extensions import assert_never
 
 
 @pytest.mark.dbfixture
@@ -37,3 +38,55 @@ def test_inventory_category_insert_data(db, category_factory, slug, is_active):
 
     assert result.slug == slug
     assert result.is_active == is_active
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, web_id, name, slug, description, is_active, created_at, updated_at"[
+        (
+            1,
+            "45425810",
+            "widstar running sneakers",
+            "widstar-running-sneakers",
+            "Lorem ipsum dolor sit amet",
+            1,
+            "2021-09-04 22:14:18",
+            "2021-09-04 22:14:18",
+        ),
+        (
+            8616,
+            "45434425",
+            "impact puse dance shoe",
+            "impact-puse-dance-shoe",
+            "Lorem ipsum dolor sit amet",
+            1,
+            "2021-09-04 22:14:18",
+            "2021-09-04 22:14:18",
+        ),
+    ],
+)
+def test_inventory_db_product_dbfixture(
+    db,
+    db_fixture_setup,
+    id,
+    web_id,
+    name,
+    slug,
+    description,
+    is_active,
+    created_at,
+    updated_at,
+):
+    result = models.Product.objects.get(id=id)
+    result_created_at = result.created_at.strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )  # time not formatted as line 52, formatted here
+
+    result_updated_at = result.updated_at.strftime("%Y-%m-%d %H:%M:%")
+    assert result.web_id == web_id
+    assert result.name == name
+    assert result.slug == slug
+    assert result.description == description
+    assert result.is_active == is_active
+    assert result.created_at == created_at
+    assert result.updated_at == updated_at
